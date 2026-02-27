4987b329a14913c168f90a8b88c20c9806530cd2
Tomohiro Ishii <tommy.ish@icloud.com>
2026-02-27 16:48:55 +0900

    chore(build): migrate from tsup to tsdown (#74)
    * build: migrate from tsup to tsdown
    
    * build: add inlineOnly to suppress unintended bundling warning

---
+++ tsdown.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/cli.ts'],
+  format: ['esm'],
+  dts: false,
+  sourcemap: false,
+  clean: true,
+  minify: false,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+  target: false,
+  outExtensions({ format }) {
+    return {
+      js: format === 'es' ? '.js' : '.cjs',
+    }
+  },
+  inlineOnly: ['@babel/parser', '@jridgewell/sourcemap-codec', 'magic-string'],
+})
